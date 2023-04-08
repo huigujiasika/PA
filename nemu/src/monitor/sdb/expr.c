8b04fc9a0291e5c6b7ae0593ce6aabeaff96d99b
tracer-ics2022 <tracer@njuics.org>
2023-04-08 17:46:28 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-QFJQBA98 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:46:28 up  1:14,  0 users,  load average: 0.00, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -301,10 +301,7 @@ word_t evalExp(int start,int end){
 
 word_t expr(char *e, bool *success) {
 
-  printf("%s",e);
-  printf("%s",e);
-  printf("%s",e);
-  
+
   if (!make_token(e)) {
     *success = false;
     return 0;
